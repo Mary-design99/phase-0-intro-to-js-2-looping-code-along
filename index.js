ca5434a89b7e7c41c3f3f2c60cab8925ea7598c9
Mary-design99 <mary.nyarangi@student.moringaschool.com>
2025-06-20 09:54:39 +0300

    loop

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(names){
+    return names.map(name=> `Thank you, ${name}, for the wonderful surprise gift!`);
+}
+function countDown(num){
+    for (let i=num;i>=0; i--){
+        console.log(i);
+    }
+}
+console.log("Counting down from 10:");
+countDown(10)
+const names=["Guadalupe","Ollie","Aki"]
+const messages=writeCards(names);
